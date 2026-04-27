89ee0654efa154f4c2e05825498d13d2e66c7bc4
v-yanxingyu <v-yanxingyu@xiaomi.com>
2026-04-27 10:13:44 +0800

    examples/camera: display captured frame on LCD framebuffer
    After capturing, write the last frame directly to /dev/fb0
    without going through the filesystem. Crop 320x240 camera
    image to center 240x240 to fit the ST7789 LCD in RPORTRAIT
    mode. Uses raw ioctl values to avoid including fb.h which
    would change binary size and trigger SPI Flash XIP deadlock.
    
    Signed-off-by: yan <yan@xiaomi.com>

--- examples/camera/camera_main.c
+++ examples/camera/camera_main.c
@@ -717,19 +717,70 @@ int main(int argc, FAR char *argv[])
                     goto exit_this_app;
                   }
 
-                futil_writeimage(
-                  (FAR uint8_t *)v4l2_buf.m.userptr,
-                  (size_t)v4l2_buf.bytesused,
-                  capture_type == V4L2_BUF_TYPE_VIDEO_CAPTURE ?
-                  "RGB" : "JPG");
+                capture_num--;
+
+                if (capture_num == 0)
+                  {
+                    int fbfd = open("/dev/fb0", O_RDWR);
+                    if (fbfd >= 0)
+                      {
+                        struct { uint8_t fmt; uint8_t p1;
+                                 uint16_t xr; uint16_t yr;
+                                 uint8_t np; } lvi;
+                        struct { void *mem; uint32_t len;
+                                 uint16_t st; uint8_t d;
+                                 uint8_t bp; } lpi;
+                        ioctl(fbfd, 0x2801, (uintptr_t)&lvi);
+                        ioctl(fbfd, 0x2802, (uintptr_t)&lpi);
+
+                        if (lpi.mem && lpi.len > 0)
+                          {
+                            FAR uint16_t *src =
+                              (FAR uint16_t *)v4l2_buf.m.userptr;
+                            FAR uint8_t *fb = lpi.mem;
+                            uint32_t stride = lpi.st > 0
+                                              ? lpi.st : 480;
+                            uint32_t r;
+                            uint32_t xo = (320 - 240) / 2;
+
+                            memset(fb, 0, lpi.len);
+                            for (r = 0; r < 240; r++)
+                              {
+                                memcpy(fb + r * stride,
+                                       (uint8_t *)(src
+                                       + r * 320 + xo),
+                                       240 * 2);
+                              }
+
+                            uint16_t area[4];
+                            area[0] = 0;
+                            area[1] = 0;
+                            area[2] = 240;
+                            area[3] = 240;
+                            ioctl(fbfd, 0x2807,
+                                  (uintptr_t)area);
+                            printf("LCD: 240x240 crop "
+                                   "(stride=%u)\n", stride);
+                          }
+
+                        close(fbfd);
+                      }
+                  }
+                else
+                  {
+                    futil_writeimage(
+                      (FAR uint8_t *)v4l2_buf.m.userptr,
+                      (size_t)v4l2_buf.bytesused,
+                      capture_type ==
+                        V4L2_BUF_TYPE_VIDEO_CAPTURE
+                        ? "RGB" : "JPG");
+                  }
 
                 ret = release_camimage(v_fd, &v4l2_buf);
                 if (ret != OK)
                   {
                     goto exit_this_app;
                   }
-
-                capture_num--;
               }
 
             ret = stop_stillcapture(v_fd, capture_type);
